a248c58e5d601688e718fd14d51799905ecbf49e
Chris Hawkins <chris.hawkins@outlook.com>
2019-06-15 11:58:43 -0700

    Added assets helper and drew a character to the screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const assets = require('./assets.js');
+
+const canvas = document.getElementById('main-canvas');
+const context = canvas.getContext('2d');
+
+assets.loadImage('images/green_blob.png').then(img => {
+    context.drawImage(img, 25, 25);
+});
