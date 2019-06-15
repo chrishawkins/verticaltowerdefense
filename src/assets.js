8d36049a8b7e4db71fa95a0d151b7e0f4dbb5b2f
Chris Hawkins <chris.hawkins@outlook.com>
2019-06-15 12:37:57 -0700

    Scale canvas and elements

--- src/assets.js
+++ src/assets.js
@@ -1,12 +1,12 @@
 export function loadImage(url) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.addEventListener('load', () => {
-            resolve(img);
-        }, false);
-        img.addEventListener('error', e => {
-            reject(e);
-        });
-        img.src = url;
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => {
+        resolve(img);
+    }, false);
+    img.addEventListener('error', e => {
+        reject(e);
     });
+    img.src = url;
+  });
 }
